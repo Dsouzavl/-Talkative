d2009931e720383e60677d9ba9b3b861c0267671
Victor Souza | Stone <vhsouza@stone.com.br>
2017-08-15 19:14:51 -0300

    DELETES OLD PROJECT.

--- src/Talkative/Models/Entities/Chat.cs
+++
@@ -1,9 +0,0 @@
-using Talkative.Models.Entities;
-
-namespace Talkative.Models.Response
-{
-    public class Chat : TelegramObject
-    {
-        public string Type { get; set; }
-    }
-}
--- src/Talkative/Models/Request/ForwardMessageRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Talkative.Models.Request
-{
-    public abstract class ForwardMessageRequest : TelegramMessage
-    {
-        public int FromChatId { get; set; }
-        public int MessageId { get; set; }
-    }
-}
